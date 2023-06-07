dcde3323e88be7526c917f858dac5e57e0086b40
ubuntu2204-upgraded <kkais786@gmail.com>
2023-06-07 23:10:45 +0500

    added pages for product categories e.g. Male, Female and Kids

---
+++ app/male/page.tsx
@@ -0,0 +1,45 @@
+import { client } from "@/lib/sanity/sanityClient";
+import Product from "@/components/product/Product";
+
+const getProductsData = async () => {
+  return await client.fetch(`*[_type=='product' && category->title=='Male']{
+    _id,
+    title,
+    description,
+    price,
+    images,
+    category->{title}
+    }`
+  );
+}
+
+interface ICategory {
+  title: string
+}
+interface IProduct {
+  _id: string,
+  title: string,
+  description: string,
+  price: number,
+  images: string[],
+  category: ICategory
+}
+
+const AllProducts = async () => {
+  const data: IProduct[] = await getProductsData();
+  // console.log(data);
+
+  return (
+    <div className="grid grid-cols-[repeat(3,auto)] justify-center gap-10">
+      {data.map((item: IProduct) =>
+        (
+          <div key={item._id}>
+            <Product item={item} />
+          </div>
+        )
+      )}
+    </div>
+  )
+}
+
+export default AllProducts
