2ec65740f662cc0689332005f2dcc6cb6b76426e
Khurram-Win11VM <kkais786@gmail.com>
2023-07-11 08:33:57 +0500

    Updated the Nav component for catching the host and protocol dynamically from headers data

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,18 +1,21 @@
-import { cookies } from "next/headers"
+import { cookies, headers } from "next/headers"
 import MainNav from "./MainNav"
 
 const getCartItems = async () => {
-  const host =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : process.env.VERCEL_URL;
+  const headersData = headers();
+  const protocol = headersData.get("x-forwarded-proto");
+  const host = headersData.get("host");
+  // const host =
+  //   process.env.NODE_ENV === 'development'
+  //     ? 'http://localhost:3000'
+  //     : process.env.VERCEL_URL;
       
   const cookieStore = cookies();
   const user_id = cookieStore.get("user_id")?.value as string;
   let noOfItems = 0
 
   try {
-    const response = await fetch(`${host}/api/cart?user_id=${user_id}`);
+    const response = await fetch(`${protocol}://${host}/api/cart?user_id=${user_id}`);
     const result = await response.json();
     noOfItems = result.length;
     // console.log(result);
