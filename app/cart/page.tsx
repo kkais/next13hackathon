ddba5bcd486d44a092d9910b2378487a2b4ac8d0
Khurram-Win11VM <kkais786@gmail.com>
2023-07-11 07:40:20 +0500

    Updated the cart page for catching the host and protocol dynamically from headers data

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,21 +2,24 @@ import Cart from "@/components/cart/Cart"
 import IOrderItem from "@/interfaces/IOrderItem";
 import IProduct from "@/interfaces/IProduct";
 import { client } from "@/lib/sanity/sanityClient";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 const getCartItems =async () => {
-  const host =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : process.env.VERCEL_URL;
+  const headersData = headers();
+  const protocol = headersData.get("x-forwarded-proto");
+  const host = headersData.get("host");
+  // const host =  
+  //   process.env.NODE_ENV === 'development'
+  //     ? 'http://localhost:3000'
+  //     : process.env.VERCEL_URL;
 
   const cookieStore = cookies();
   const user_id = cookieStore.get("user_id")?.value as string;
-  const url = `${host}/api/cart?user_id=${user_id}`; 
+  const url = `${protocol}://${host}/api/cart?user_id=${user_id}`; 
   let orderItems: IOrderItem[] = [];
     
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {method: 'GET'});
     const result = await response.json();
     //console.log("cartItems:", result);
 
